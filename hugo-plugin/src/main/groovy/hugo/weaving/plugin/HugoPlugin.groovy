6a438c9de3c7ec98b2ddbfa5f5143882c18d6334
JuanLv <olbbme@gmail.com>
2017-03-02 12:35:27 +0800

    update version name

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -26,10 +26,10 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.github.olbb.hugo:hugo-runtime:1.2.2-SNAPSHOT1'
+      debugCompile 'com.github.olbb.hugo:hugo-runtime:1.2.2s'
       // TODO this should come transitively
       debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.github.olbb.hugo:hugo-annotations:1.2.2-SNAPSHOT1'
+      compile 'com.github.olbb.hugo:hugo-annotations:1.2.2'
     }
 
     project.extensions.create('hugo', HugoExtension)
